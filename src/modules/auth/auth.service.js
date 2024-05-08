7fcd4fc39437f7f3c6acac9d7d20005d7a18ba73
Sthery <sthery.styll@gmail.com>
2024-05-08 12:49:04 +0100

    Router Register

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -17,4 +17,13 @@ export const login = async (params) => {
     return { token };
 };
 
-export const register = async (params) => { };
+export const register = async (params) => {
+    const user = await getByEmail(params.email);
+    if (user) {
+        return { error: 'This e-mail already exists ' };        
+    }
+    const userCreated = await save(params);
+
+    const token = jwt.sign({ id: userCreated[0] }, process.env.JWT_SECRET);
+    return { token };
+};
