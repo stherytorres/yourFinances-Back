2a8feeda15d8b06ce272dd2afefdc9cbb00dfeb8
Sthery <sthery.styll@gmail.com>
2024-05-17 10:43:12 +0100

    Componente Transações

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import userRouter from './modules/user/user.route.js';
 import authRouter from './modules/auth/auth.route.js';
 import categoriaRouter from './modules/categorias/categoria.route.js';
 import metaRouter from './modules/metas/meta.route.js';
+import transacaoRouter from './modules/transacoes/transacao.route.js';
 
 const app = express();
 app.use(express.json());
@@ -17,6 +18,7 @@ app.use('/users', userRouter);
 app.use('/auth', authRouter);
 app.use('/categorias', categoriaRouter);
 app.use('/metas', metaRouter);
+app.use('/transacoes', transacaoRouter);
 
 app.get('/health', (_, res) => {
     return res.send('Sistema está operacional!');
